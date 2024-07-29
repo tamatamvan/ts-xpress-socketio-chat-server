c82cee64e65c55926ff5f03fdaf3e03cd4c250c0
Septian A Tama <sfirst.tama@gmail.com>
2024-07-29 20:43:28 +0700

    create simple socket.io server

---
+++ index.ts
@@ -0,0 +1,36 @@
+import express, { Express, Request, Response } from 'express';
+import { Server } from 'socket.io';
+import { createServer } from 'http';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+type Message = { author: string; content: string };
+
+const app: Express = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: 'http://localhost:5173',
+  },
+});
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('It works!');
+});
+
+io.on('connection', (socket) => {
+  socket.on('new-user', (name) => {
+    socket.emit('user-connected', name);
+  });
+  socket.on('message', (message: Message) => {
+    socket.emit('message', message);
+  });
+});
+
+httpServer.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
